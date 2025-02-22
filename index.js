78b60f3445cb523449e738fc56882d1bee8d6b88
longthanhdam <damthanhlong@gmail.com>
2025-02-22 09:04:04 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Jenkins and Kubernetes!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
